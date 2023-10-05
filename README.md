fa694d5f4fbbdf80b73791aca1cba523c2665eed
Jeronimo Diogo (XC-AD/ENG62-Brg) <Diogo.Pereira4@pt.bosch.com>
2023-10-05 16:21:58 +0100

    Better unit test and readme update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ci-cd-experimentation
+
+![Tests](https://github.com/diogoje/ci-cd-experimentation.git/actions/workflows/python-package.yml/badge.svg)
--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,5 +1,12 @@
+from unittest.mock import MagicMock
+
+import pytest
+
 from hello.hello_world import print_hello_world
 
 
-def test_hello_world():
-    assert not print_hello_world()
+def test_hello_world(monkeypatch: pytest.MonkeyPatch):
+    print_mock = MagicMock()
+    monkeypatch.setattr("builtins.print", print_mock)
+    print_hello_world()
+    print_mock.assert_called_with("Hello world!")
